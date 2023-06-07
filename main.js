9d7c401cdb617973bcd40d4236f1fce4acbac4c1
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2023-06-07 17:26:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("my first __________ in console");
+
+var firstName = "jonh";
+
+var lastName = "Ignácio";
+
+var fullName = firstName.concat(lastName);
+
+console.log(fullName);
+
+var a = 420;
+var b = 70;
+var add = a + b;
+console.log(add);
+var sub = a - b;
+console.log(sub);
+var mul = a * b;
+console.log(mul);
+var div = a / b;
+console.log(div);
